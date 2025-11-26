beed60b2042f9d59511e2234d80408087e1d0f14
ephykal <ephraimolu6@gmail.com>
2025-11-26 21:49:01 +0100

    created table and customer-review functionalities

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css'
 import MainDash from './components/MainDash/MainDash';
+import RightSide from './components/RightSide/RightSide';
 import Sidebar from './components/Sidebar/Sidebar';
 
 
@@ -10,6 +11,7 @@ function App() {
       <div className='AppGlass'>
         <Sidebar />
         <MainDash />
+        <RightSide />
       </div>
 
     </div>
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,41 +1,67 @@
 import React, { useState } from 'react';
 import './Sidebar.css';
 import Logo from '../../imgs/logo.png';
-import { SidebarData } from '../../Data/Data';
 import { UilSignOutAlt } from '@iconscout/react-unicons';
+import { SidebarData } from '../../Data/Data';
+import { UilBars } from '@iconscout/react-unicons';
+import { motion } from 'framer-motion';
 
 const Sidebar = () => {
 	const [selected, setSelected] = useState(0);
 
-	return (
-		<div className='Sidebar'>
-			{/* logo */}
-			<div className='logo'>
-				<img src={Logo} alt='logo image' srcset='' />
-				<span>
-					Sh<span>o</span>ps
-				</span>
-			</div>
+	const [expanded, setExpaned] = useState(true);
 
-			{/* menu */}
-			<div className='menu'>
-				{SidebarData.map((item, index) => {
-					return (
-						<div
-							className={selected === index ? 'menuItem  active' : 'menuItem'}
-							key={index}
-							onClick={() => setSelected(index)}
-						>
-							<i>{React.createElement(item.icon)}</i>
-							<span>{item.heading}</span>
-						</div>
-					);
-				})}
-				<div className='menuItem'>
-					<UilSignOutAlt />
-				</div>
+	const sidebarVariants = {
+		true: {
+			left: '0',
+		},
+		false: {
+			left: '-60%',
+		},
+	};
+	console.log(window.innerWidth);
+	return (
+		<>
+			<div
+				className='bars'
+				style={expanded ? { left: '60%' } : { left: '5%' }}
+				onClick={() => setExpaned(!expanded)}
+			>
+				<UilBars />
 			</div>
-		</div>
+			<motion.div
+				className='sidebar'
+				variants={sidebarVariants}
+				animate={window.innerWidth <= 768 ? `${expanded}` : ''}
+			>
+				{/* logo */}
+				<div className='logo'>
+					<img src={Logo} alt='logo' />
+					<span>
+						Sh<span>o</span>ps
+					</span>
+				</div>
+
+				<div className='menu'>
+					{SidebarData.map((item, index) => {
+						return (
+							<div
+								className={selected === index ? 'menuItem active' : 'menuItem'}
+								key={index}
+								onClick={() => setSelected(index)}
+							>
+								<item.icon />
+								<span>{item.heading}</span>
+							</div>
+						);
+					})}
+					{/* signoutIcon */}
+					<div className='menuItem'>
+						<UilSignOutAlt />
+					</div>
+				</div>
+			</motion.div>
+		</>
 	);
 };
 
